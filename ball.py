7409474b53e58aa7dfb503e414ae13bcddba1ddd
admjorgeluiz <jorgeluiz.adm@outlook.com>
2025-05-21 11:11:06 -0300

    Atualização Henrique

--- ball.py
+++ ball.py
@@ -1,31 +1,43 @@
 import pygame
 from random import randint
 
-
 # Classe representando a bola
 class Ball(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
-
+        self.normal_color = pygame.Color(color)
+        self.glow_color = pygame.Color(255, 255, 0)  # Cor de brilho (amarelo)
+        self.width = width
+        self.height = height
         self.image = pygame.Surface([width, height])
         self.image.fill("black")
         self.image.set_colorkey("black")
 
-        pygame.draw.circle(self.image, color, (width // 2, height // 2), width // 2)
+        self.glow_timer = 0  # Duração do brilho (em frames)
+        self.draw_ball(self.normal_color)
+
         self.rect = self.image.get_rect()
         self.velocity = [randint(3, 8), randint(-15, 15)]
+
+    def draw_ball(self, color):
+        self.image.fill("black")
+        pygame.draw.circle(self.image, color, (self.width // 2, self.height // 2), self.width // 2)
 
     def update(self):
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
 
+        if self.glow_timer > 0:
+            self.glow_timer -= 1
+            self.draw_ball(self.glow_color)
+        else:
+            self.draw_ball(self.normal_color)
+
     def bounce(self):
-        self.change_color((0, 255, 0))
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = randint(-15, 15)
+        self.glow_timer = 10  # Brilho por 10 frames (~0.16s a 60 FPS)
 
     def change_color(self, color):
-        width = self.image.get_width()
-        height = self.image.get_height()
-        self.image.fill("black")
-        pygame.draw.circle(self.image, color, (width // 2, height // 2), width // 2)
+        self.normal_color = pygame.Color(color)
+        self.draw_ball(self.normal_color)
